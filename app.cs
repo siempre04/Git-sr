f9b7361580da0107c1061f90c12f6bd2503bce2d
siempre04 <slepuhinde@gmail.com>
2026-04-30 23:24:08 +0500

    Добавен метод умножения и его вызов

--- app.cs
+++ app.cs
@@ -9,9 +9,16 @@ class App
         return a - b;
     }
 
+    static int Multiply(int a,int b)
+    {
+        return a * b
+    }
     static void Main()
     {
         int result = Subtract(30, 12);
         Console.WriteLine("Разность: " + result);
+
+        int result = Multiply(5, 2);
+        Console.WriteLine("Произведение: " + product);
     }
 }
