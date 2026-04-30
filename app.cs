5cf7ca2d9363e3cd79a54a26e4fe712318d8c993
siempre04 <slepuhinde@gmail.com>
2026-04-30 23:32:49 +0500

    Сохранение перед обьединением в гитхабе

--- app.cs
+++ app.cs
@@ -3,23 +3,15 @@ using System;
 
 class App
 {
-    Console.WriteLine("Калькулятор v2.0-FINAL ");
+    Console.WriteLine("Калькулятор v2.0-FINAL");
     static int Subtract(int a, int b)
     {
         return a - b;
     }
 
-    static int Multiply(int a,int b)
-    {
-        return a * b
-    }
     static void Main()
     {
         int result = Subtract(30, 12);
         Console.WriteLine("Разность: " + result);
-
-        int result = Multiply(5, 2);
-        Console.WriteLine("Произведение: " + product);
-
     }
 }
