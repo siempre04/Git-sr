55c97b4f73be7882170564ff1fc8dfab5f71b879
siempre04 <slepuhinde@gmail.com>
2026-04-30 23:00:04 +0500

    Добавил метод subtract и его вызов для чисел 30 и 12

--- app.cs
+++ app.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;  
+
+class App
+{
+    static int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+
+    static void Main()
+    {
+        int result = Subtract(30, 12);
+        Console.WriteLine("Разность: " + result);
+    }
+}
